03eed7650a67aceb278f0252461ca2ecfadc4f3f
ashismaji2025 <majiashiskumar390@gmail.com>
2025-06-24 08:00:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import logging
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import logging
 
 logging.basicConfig(level=logging.INFO)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hello! I am AshisF&Obot.")
+# Your new working bot token
+TOKEN = "7526432651:AAEFgsQngsHUxp0r6VIcMvJkBDPVAkXkxMc"
 
-app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hello Ashis-da! Your AshisF&Obot is now active 💹")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
