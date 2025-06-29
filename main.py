983caf966a8bd98544261f9f2cfa34a0a79c7666
ashismaji2025 <majiashiskumar390@gmail.com>
2025-06-29 18:40:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello Ashis-da! 💹 Your F&O bot is now working 💖")
