21fe6f387e8ae4452c82a82f81b33ad61dc37042
ashismaji2025 <majiashiskumar390@gmail.com>
2025-06-29 12:35:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "7526432651:AAGpowkLKalWPw2w9pjzp5Hm3G797DS9p74"  # Directly use token
+TOKEN = "7526432651:AAGpowkLKalWPw2w9pjzp5Hm3G797DS9p74"  # Use your real token
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello Ashis-da! Your AshisFNObot is now active 💹")
