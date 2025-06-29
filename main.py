1ad6fcfc880af19cb15d4450f697509c241aec8c
ashismaji2025 <majiashiskumar390@gmail.com>
2025-06-29 10:14:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello Ashis-da! Your AshisFNObot is now active 💹")
 
 def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))  # This keeps Flask running
+
     application = ApplicationBuilder().token(TOKEN).build()
-
     application.add_handler(CommandHandler("start", start))
-
     application.run_polling()
 
 if __name__ == "__main__":
