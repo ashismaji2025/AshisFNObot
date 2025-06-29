64f09b2f7b40a5d27693b2c00fe2ef8e2c4d7f45
ashismaji2025 <majiashiskumar390@gmail.com>
2025-06-29 18:28:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello Ashis-da! 💹 Your F&O bot is now live and working 💖")
+    await update.message.reply_text("Hello Ashis-da! 💹 Your F&O bot is now working 💖")
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
