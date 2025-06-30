d28e40d8dde0c0b2c71f3f5886c648388b1a843f
ashismaji2025 <majiashiskumar390@gmail.com>
2025-06-30 18:25:41 +0530

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,11 +1,17 @@
-# signals.py
-
 from datetime import datetime
 
-def generate_demo_signal():
-    now = datetime.now().strftime("%H:%M:%S")
-    
-    return f"📢 *Buy on dips* Nifty Future-July 💹\n" \
-           f"*CMP:* 23450\n" \
-           f"*Stop Loss:* 23320\n" \
-           f"*Time:* {now}"
+def get_sample_signal():
+    now = datetime.now().strftime("%d-%b-%Y %H:%M:%S")
+    signal = f"""📈 *AshisF&Obot Status* [{now}]
+
+🔹 *Instrument:* Nifty Future-Jun
+🔹 *Trend:* Bullish
+🔹 *Strategy:* Buy on dips
+🔹 *CMP:* 23455
+🔹 *Stoploss:* 23305
+🔹 *Target:* 23620
+🔹 *Style:* Intraday
+🔹 *Note:* Avoid trade if 23305 breaks.
+
+🧠 Signal by Ràñi's engine."""
+    return signal
